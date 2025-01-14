ab7976b97a42e406ad2de76b17a88101dae48ffb
yushmitha <yushmitha79@gmail.com>
2025-01-14 14:00:59 +0530

    dark theme added

--- backgroundStyling/src/app/app.component.ts
+++ backgroundStyling/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ColorPickerComponent } from "./color-picker/color-picker.component";
 
 @Component({
   selector: 'bsa-root',
-  imports: [RouterOutlet, ColorPickerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'backgroundStyling';
+  theme = 'dark';
 }
