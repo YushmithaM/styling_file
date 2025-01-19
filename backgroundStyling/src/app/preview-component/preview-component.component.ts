16e112440c5f47a59824bc9a12ca5582d5c03e0b
yushmitha <yushmitha79@gmail.com>
2025-01-19 19:24:52 +0530

    added preview component

---
+++ backgroundStyling/src/app/preview-component/preview-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-component',
+  imports: [],
+  templateUrl: './preview-component.component.html',
+  styleUrl: './preview-component.component.scss'
+})
+export class PreviewComponentComponent {
+
+}
